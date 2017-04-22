8f4d379eb8fe180f2a73f444a7f79015d9de1260
Gonzalo Fernández <gonzalo.fernandez.pico@gmail.com>
2017-04-22 10:30:07 +0200

    Abstract class

--- app/src/main/java/xyz/gonzapico/myfirstkotlinapp/Person.kt
+++ app/src/main/java/xyz/gonzapico/myfirstkotlinapp/Person.kt
@@ -3,22 +3,11 @@ package xyz.gonzapico.myfirstkotlinapp
 /**
  * Created by gfernandez on 22/04/17.
  */
-open class Person(val name: String, val age: Int)
-
+abstract class Person(val name: String, val age: Int)
+// Open by default
 class Developer(name: String) : Person(name, 24)
 
 // Without constructor
 class MyDeveloper(val name: String = "", val age: Int = 20) {
 
-}
-
-fun testClass(){
-  val myDev = MyDeveloper()
-  // name = "", age = 20
-  val myDevTwo  = MyDeveloper("U")
-  // name = "U", age = 20
-  val myDevThree = MyDeveloper("U", 25)
-  // name = "U", age = 25
-  val myDevFour = MyDeveloper(age = 36)
-  // name = "", age = 36
 }
