6f1e1a24979800eeaaa01cc4218993fbb1fa01ce
Gonzalo Fernández <gonzalo.fernandez.pico@gmail.com>
2017-04-22 10:18:36 +0200

    Created class as Java style

---
+++ app/src/main/java/xyz/gonzapico/myfirstkotlinapp/Person.kt
@@ -0,0 +1,18 @@
+package xyz.gonzapico.myfirstkotlinapp
+
+/**
+ * Created by gfernandez on 22/04/17.
+ */
+open class Person(name:String, age:Int){
+  // Properties
+  val name : String
+  val age : Int
+
+  // Executing when creating the class
+  init {
+    this.name = name
+    this.age = age
+  }
+}
+
+class Developer : Person()
