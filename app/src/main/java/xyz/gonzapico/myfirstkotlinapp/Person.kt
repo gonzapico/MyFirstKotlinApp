89a43500ecd63d8f361b84757b9c5a044513c935
Gonzalo Fernández <gonzalo.fernandez.pico@gmail.com>
2017-04-22 10:35:28 +0200

    Added custom get and set

--- app/src/main/java/xyz/gonzapico/myfirstkotlinapp/Person.kt
+++ app/src/main/java/xyz/gonzapico/myfirstkotlinapp/Person.kt
@@ -3,7 +3,20 @@ package xyz.gonzapico.myfirstkotlinapp
 /**
  * Created by gfernandez on 22/04/17.
  */
-class Person(var name: String, var age: Int)
+class Person(name: String, age: Int){
+  // To change functionality in getter and/or setter
+
+  var name = name
+    get() = "Name -> $field"
+    set(value){
+      if (value.isNotEmpty()){
+        field = value
+      }
+    }
+  var age = age
+
+
+}
 
 fun testClass(){
   val person = Person("Anto", 33)
