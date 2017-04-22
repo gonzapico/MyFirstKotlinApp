674892439351155bbcfb602433afdaa8b013f907
Gonzalo Fernández <gonzalo.fernandez.pico@gmail.com>
2017-04-22 12:28:09 +0200

    Functions by extensions

--- app/src/main/java/xyz/gonzapico/myfirstkotlinapp/Extensions.kt
+++ app/src/main/java/xyz/gonzapico/myfirstkotlinapp/Extensions.kt
@@ -1,7 +1,10 @@
 package xyz.gonzapico.myfirstkotlinapp
 
 import android.content.Context
+import android.support.annotation.LayoutRes
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import android.widget.Toast
 
 /**
@@ -11,3 +14,6 @@ fun Context.showToast(message: String) = // this = Context
     Toast.makeText(this, message, Toast.LENGTH_LONG).show()
 
 fun RecyclerView.ViewHolder.showToast(message: String) = this.itemView.context.showToast(message)
+
+fun ViewGroup.inflate(@LayoutRes layoutToInflate: Int) = LayoutInflater.from(context).inflate(
+    layoutToInflate, this, false)
--- app/src/main/java/xyz/gonzapico/myfirstkotlinapp/MediaAdapter.kt
+++ app/src/main/java/xyz/gonzapico/myfirstkotlinapp/MediaAdapter.kt
@@ -1,7 +1,6 @@
 package xyz.gonzapico.myfirstkotlinapp
 
 import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -15,8 +14,7 @@ class MediaAdapter(
     val mediaItemList: List<MediaItem>) : RecyclerView.Adapter<MediaAdapter.MediaViewHolder>() {
 
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MediaViewHolder {
-    val view = LayoutInflater.from(parent.context).inflate(R.layout.view_media_item, parent, false)
-    return MediaViewHolder(view)
+    return MediaViewHolder(parent.inflate(R.layout.view_media_item))
   }
 
   override fun onBindViewHolder(holder: MediaViewHolder, position: Int) {
