fc8fe321ee77b532a540c391f7cd229b13fe68e8
Gonzalo Fernández <gonzalo.fernandez.pico@gmail.com>
2017-04-22 10:12:21 +0200

    OnClickListener just improved

--- app/src/main/java/xyz/gonzapico/myfirstkotlinapp/HomeActivity.kt
+++ app/src/main/java/xyz/gonzapico/myfirstkotlinapp/HomeActivity.kt
@@ -2,10 +2,8 @@ package xyz.gonzapico.myfirstkotlinapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 
 class HomeActivity : AppCompatActivity() {
@@ -15,27 +13,12 @@ class HomeActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_home)
 
-    val tvWelcomeMessage = findViewById(R.id.tvWelcomeMessage) as TextView
     val btnShowMessage = findViewById(R.id.btnShowMessage) as Button
     val etMessage = findViewById(R.id.etMessage) as EditText
 
-    btnShowMessage.setOnClickListener(object  : View.OnClickListener{
-      override fun onClick(v: View?) {
-        showToastMessage("Message :  ${etMessage.text}")
-      }
+    btnShowMessage.setOnClickListener { showToastMessage("Hello ${etMessage.text}") }
 
-    })
-//    btnShowMessage.setOnClickListener { showToastMessage("Hello ${etMessage.text}") }
-
-//    tvWelcomeMessage.setText(resources.getString(R.string.welcomeMessage))
-
-//    showToastMessage("Hello  ${tvWelcomeMessage.text}")
   }
 
-  // Normal use
-//  fun showToastMessage(message : String) = Toast.makeText(this, message, Toast.LENGTH_LONG).show()
-
-  // Assigning value directly
   fun showToastMessage(message: String) = Toast.makeText(this, message, Toast.LENGTH_LONG).show()
-  // Assign to the function the response (it doesn't exists void, you can use Unit instead)
 }
