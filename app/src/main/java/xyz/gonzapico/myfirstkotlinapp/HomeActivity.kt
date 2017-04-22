f10049cef82c7294f4cc2c66cf9738d5f99fac98
Gonzalo Fernández <gonzalo.fernandez.pico@gmail.com>
2017-04-22 09:49:15 +0200

    Change message of a textview in code

--- app/src/main/java/xyz/gonzapico/myfirstkotlinapp/HomeActivity.kt
+++ app/src/main/java/xyz/gonzapico/myfirstkotlinapp/HomeActivity.kt
@@ -1,12 +1,18 @@
 package xyz.gonzapico.myfirstkotlinapp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
+
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_home)
+
+    val tvWelcomeMessage = findViewById(R.id.tvWelcomeMessage) as TextView
+
+    tvWelcomeMessage.setText(resources.getString(R.string.welcomeMessage))
   }
 }
