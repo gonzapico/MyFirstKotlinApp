92606b7db16e0aa4502f673410dbd39c76a44753
Gonzalo Fernández <gonzalo.fernandez.pico@gmail.com>
2017-04-22 10:26:50 +0200

    Extra constructor, not usual

--- app/src/main/java/xyz/gonzapico/myfirstkotlinapp/Person.kt
+++ app/src/main/java/xyz/gonzapico/myfirstkotlinapp/Person.kt
@@ -3,20 +3,16 @@ package xyz.gonzapico.myfirstkotlinapp
 /**
  * Created by gfernandez on 22/04/17.
  */
-open class Person(val name:String, val age:Int)
+open class Person(val name: String, val age: Int)
 
 class Developer(name: String) : Person(name, 24)
 
 // Without constructor
-class MyDeveloper{
-  val name : String
-  val age : Int
-  constructor(name: String){
-    this.name = name
-    this.age = 20
-  }
-  constructor(age: Int){
-    this.name = "U"
+class MyDeveloper(val name: String) {
+
+  var age: Int = 20
+
+  constructor(age: Int) : this("") {
     this.age = age
   }
 
