d9437dc2e9c584f2b010c21671774cf864a6c6c7
Gonzalo Fernández <gonzalo.fernandez.pico@gmail.com>
2017-04-22 17:14:39 +0200

    Added an observable to notify the data changes on the adapter

--- app/src/main/java/xyz/gonzapico/myfirstkotlinapp/HomeActivity.kt
+++ app/src/main/java/xyz/gonzapico/myfirstkotlinapp/HomeActivity.kt
@@ -2,26 +2,9 @@ package xyz.gonzapico.myfirstkotlinapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.RecyclerView
-import android.util.Log
-import android.view.View
-import android.view.ViewGroup
-import android.widget.FrameLayout
-import kotlin.properties.Delegates
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
-
-  val f: (Int) -> Int = { x ->
-    x * x
-  }
-
-  val recycler by lazy { find<RecyclerView>(R.id.recycler) }
-
-  var observedNumber by Delegates.observable(0) {
-    // What we are going to do every time observedNumber is changed
-    p, old, new ->
-    Log.d("", "old -> $old & new -> $new")
-  }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -29,15 +12,12 @@ class HomeActivity : AppCompatActivity() {
 
     // Call the function find just the first time that the view is inflated. Next, it should be cached
     // id = recycler
-    recycler.adapter = MediaAdapter(fetchMedia()) {
+    val adapter = MediaAdapter() {
       // This is valid just if there is one input parameter
       showToast(it.title)
     }
-
-    val viewGroup: ViewGroup = FrameLayout(this)
-    val list: List<View> = viewGroup.filterViews(View::visible)
-
-    observedNumber = 20
+    recycler.adapter = adapter
+    adapter.mediaItemList = fetchMedia()
   }
 
 }
--- app/src/main/java/xyz/gonzapico/myfirstkotlinapp/MediaAdapter.kt
+++ app/src/main/java/xyz/gonzapico/myfirstkotlinapp/MediaAdapter.kt
@@ -1,17 +1,22 @@
 package xyz.gonzapico.myfirstkotlinapp
 
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.view_media_item.view.*
+import kotlin.properties.Delegates
 import kotlinx.android.synthetic.main.view_media_item.view.media_title as mediaTitle
 
 /**
  * Created by gfernandez on 22/04/17.
  */
 class MediaAdapter(
-    val mediaItemList: List<MediaItem>,
     val listener: (MediaItem) -> Unit) : RecyclerView.Adapter<MediaAdapter.MediaViewHolder>() {
+
+  var mediaItemList: List<MediaItem> by Delegates.observable(emptyList()){
+    _, _, _ -> notifyDataSetChanged()
+  }
 
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MediaViewHolder {
     return MediaViewHolder(parent.inflate(R.layout.view_media_item))
