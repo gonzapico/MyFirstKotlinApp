e8253761f7dfad1bab5afdfc27af632cbc9c45ac
Gonzalo Fernández <gonzalo.fernandez.pico@gmail.com>
2017-04-22 17:01:53 +0200

    Property delegation: lazy

--- app/src/main/java/xyz/gonzapico/myfirstkotlinapp/HomeActivity.kt
+++ app/src/main/java/xyz/gonzapico/myfirstkotlinapp/HomeActivity.kt
@@ -2,10 +2,10 @@ package xyz.gonzapico.myfirstkotlinapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
-import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
@@ -13,10 +13,13 @@ class HomeActivity : AppCompatActivity() {
     x * x
   }
 
+  val recycler by lazy { find<RecyclerView>(R.id.recycler) }
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_home)
 
+    // Call the function find just the first time that the view is inflated. Next, it should be cached
     // id = recycler
     recycler.adapter = MediaAdapter(fetchMedia()) {
       // This is valid just if there is one input parameter
